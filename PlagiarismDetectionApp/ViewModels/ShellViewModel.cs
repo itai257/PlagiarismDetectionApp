98bb54f4d39f7529699817cc649dcc4d02e00018
imalka <itai257@gmail.com>
2019-02-07 13:49:50 +0200

    Added more UI open folder grid rearrange etc...

--- PlagiarismDetectionApp/ViewModels/ShellViewModel.cs
+++ PlagiarismDetectionApp/ViewModels/ShellViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Commands;
+using System.Windows.Forms;
 using Prism.Mvvm;
 
 namespace PlagiarismDetectionApp.ViewModels
@@ -10,5 +12,41 @@ namespace PlagiarismDetectionApp.ViewModels
     class ShellViewModel : BindableBase
     {
         public string Text { set; get; } = "textesgsjnhgsd";
+
+        private string corpusPath;
+
+        public string CorpusPath
+        {
+            get
+            {
+                return corpusPath;
+            }
+            set
+            {
+                corpusPath = value;
+                RaisePropertyChanged(nameof(CorpusPath));
+            }
+        }
+
+        public DelegateCommand TextFieldFocusedCommand { get; }
+
+        public ShellViewModel()
+        {
+            TextFieldFocusedCommand = new DelegateCommand(OpenFileBrowser);
+        }
+
+        private void OpenFileBrowser()
+        {
+                OpenFileDialog();
+        }
+
+        private void OpenFileDialog()
+        {
+            var openFolderDialog = new FolderBrowserDialog();
+            if (openFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                CorpusPath = openFolderDialog.SelectedPath;
+            }
+        }
     }
 }
