f72b8017df6608e06eae7b96106a719e1f30e0e9
Idan Malka <idanm@codevalue.net>
2019-02-08 22:32:54 +0200

    Added about window with navigation

--- PlagiarismDetectionApp/ViewModels/ShellViewModel.cs
+++ PlagiarismDetectionApp/ViewModels/ShellViewModel.cs
@@ -1,18 +1,56 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using Prism.Mvvm;
 using PlagiarismDetectionApp.Views;
+using Prism.Commands;
 using Prism.Regions;
 
 namespace PlagiarismDetectionApp.ViewModels
 {
     class ShellViewModel : BindableBase
     {
-        public string Text { set; get; } = "textesgsjnhgsd";
         private IRegionManager regionManager;
+        private bool _showAbout = false;
+        private bool _showMainView;
+
+        public string Text { set; get; } = "textesgsjnhgsd";
+        public DelegateCommand ShowAboutViewCommand { get; set; }
+        public DelegateCommand GoBackCommand { get; set; }
+
+        public bool ShowAboutView
+        {
+            get => _showAbout;
+            set => SetProperty(ref _showAbout, value);
+        }
+
+        public bool ShowMainView
+        {
+            get => _showMainView;
+            set => SetProperty(ref _showMainView, value);
+        }
 
         public ShellViewModel(MainWindowView mainWindowView, IRegionManager regionManager)
         {
             this.regionManager = regionManager;
             regionManager.RegisterViewWithRegion("MainRegion", typeof(MainWindowView));
+            ShowAboutViewCommand = new DelegateCommand(OnShowAbout);
+            GoBackCommand = new DelegateCommand(OnGoBack);
+            ShowMainView = true;
+            ShowAboutView = false;
+
+        }
+
+        private void OnShowAbout()
+        {
+            ShowMainView = false;
+            ShowAboutView = true;
+            regionManager.RequestNavigate("MainRegion", "AboutView");
+        }
+
+        private void OnGoBack()
+        {
+            ShowAboutView = false;
+            ShowMainView  = true;
+            regionManager.RequestNavigate("MainRegion", "MainWindowView");
         }
     }
 }
